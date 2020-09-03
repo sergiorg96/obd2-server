9dca994d7ab0c713499df69e1ef13450e5c70c79
sergiorg96 <sergioromangonzalez1996@gmail.com>
2020-09-03 19:09:41 +0200

    Eliminado la vista de usuarios que estaba de prueba
    Signed-off-by: sergiorg96 <sergioromangonzalez1996@gmail.com>

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 var vehiclesRouter = require('./routes/vehicles');
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/vehicles', vehiclesRouter);
 
 
